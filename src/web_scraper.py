4c1f2f4e6acdc1fcee44d1e24c52763b597655c7
vicenc <vicencpio@gmail.com>
2021-04-01 10:49:12 +0200

    Afegides funcions d'extracció de dades amb webdriver
    Modificada funció descàrrega json

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -1,13 +1,23 @@
 import requests
 import io
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-MAX_OFFSET = 21500
+#region Global variables
 web = 'https://analisi.transparenciacatalunya.cat/Salut/Registre-de-casos-de-COVID-19-realitzats-a-Catalun/jj6z-iyrp/data'
 web_paginacio = 'https://analisi.transparenciacatalunya.cat/api/id/jj6z-iyrp.json?$query=select%20*%2C%20%3Aid%20offset%20200%20limit%20100'
 fitxer_web = "../scraping/web.html"
 fitxer_dades = '../scraping/dades_paginacio.json'
+driver_location = "C:/Users/vicen/Downloads/chromedriver.exe"
+#endregion
 
+"""
+Starts webdriver
+"""
+# driver = webdriver.Chrome(driver_location)
+# driver.get(web)
+
+#region Private functions
 def get_web_content(url):
     """
     Return web content from url
@@ -27,8 +37,42 @@ def write_to_file(content, filename, mode):
         f.close()
     except:
         print("Error with file: {}".format(filename))
+#endregion
 
-# Dades web
+#region Webdriver functions
+"""
+Extract data using web driver
+"""
+def get_first_rows(nrows):
+    """
+    Returns first n rows
+    """
+    if nrows > 0:
+        for r in range(1, nrows + 1):
+            el_list = []
+            for i in range(1, 12):
+                element = driver.find_element_by_xpath("//div[@class='socrata-table frozen-columns']/table/tbody/tr["+ str(r) +"]/td["+ str(i) +"]/div").text
+                el_list.append(element)
+            print(el_list)
+    else:
+        print("El valor ha de ser mínim 1")
+
+def get_column(n, column):
+    """
+    Return first n registers of selected column
+    """
+    if column > 0 and column < 12:
+        if n > 0:
+            for i in range(1, n + 1):
+                table = driver.find_element_by_xpath("//div[@class='socrata-table frozen-columns']/table/tbody/tr["+ str(i) +"]/td["+ str(column) +"]/div").text
+                print(table)
+        else:
+            print("El valor ha de ser mínim 1")
+    else:
+        print("El valor ha d'estar entre 1 i 11")
+#endregion
+
+#region Download content
 def download_web():
     """
     Save web content to file web.html
@@ -39,54 +83,43 @@ def download_web():
 
 # download_web()
 
-
-# Dades paginació
-def download_data():
+def download_data(npages):
     """
-    Save paging content to file dades_paginacio.json
+    Save n pages of content to file dades_paginacio.json
     """
+    if npages > 0:
+        url_web = web_paginacio
+        offset = 20200
+        max_offset = offset + (npages * 100) - 100
 
-    offset = 20200
-    next_page = True
-    
-    fixed_html = get_web_content(web_paginacio)
-    print("Data downloaded to dades_paginacio.json, offset: {}".format(offset))
-    fixed_html = fixed_html.replace(']', ',')
-    write_to_file(fixed_html, fitxer_dades, 'a')
+        try:
+            fixed_html = get_web_content(url_web)
+            print("Data downloaded to dades_paginacio.json, offset: {}".format(offset))
+            if offset < max_offset:
+                fixed_html = fixed_html.replace(']', ',')
 
-    # while len(fixed_html2[:-3]) > 0:
-    while next_page:
-        #Descarreguem els següents 100 resultats
-        web_paginacio.replace(str(offset), str(offset + 100))
-        offset += 100
-        fixed_html = get_web_content(web_paginacio)
-        print("Data downloaded to dades_paginacio.json, offset: {}".format(offset))
-        fixed_html = fixed_html.replace('[', '')
+            write_to_file(fixed_html, fitxer_dades, 'w')
 
-        if offset < MAX_OFFSET:
-            fixed_html = fixed_html.replace(']', ',')
-        else:
-            next_page = False
+            while offset < max_offset:
+                #Descarreguem els següents 100 resultats
+                url_web = url_web.replace(str(offset), str(offset + 100))
+                offset += 100
+                fixed_html = get_web_content(url_web)
+                print("Data downloaded to dades_paginacio.json, offset: {}".format(offset))
+                fixed_html = fixed_html.replace('[', '')
 
-        write_to_file(fixed_html, fitxer_dades, 'a')
+                if offset < max_offset:
+                    fixed_html = fixed_html.replace(']', ',')
+                    write_to_file(fixed_html, fitxer_dades, 'a')
+                else:
+                    write_to_file(fixed_html, fitxer_dades, 'a')
+                    break
 
-# download_data()        
+        except Exception as e:
+            print(e)
 
-def navigate_tags():
-    """
-    Navigate for tags structure
-    """
-    page = requests.get(web)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    web_cont = soup.prettify()
+    else:
+        print("El valor ha de ser mínim 1")
 
-    head_tag = soup.head
-    print("head tag: ".format(head_tag))
-
-    # title_tag = head_tag.contents[0]
-
-    a_tag = soup.find_all('a')
-    # No se que fer ni com navegar per a_tag
-    print("a_tag: {}".format(a_tag))
-
-# navigate_tags()
+# download_data(3)
+#endregion
